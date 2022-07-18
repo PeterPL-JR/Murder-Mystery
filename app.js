58025169c37dbc0737bfcc5e9d94dfaa55aecb2c
PeterPL-JR <piokaf05@gmail.com>
2022-07-18 18:34:47 +0200

    Player Animations

--- app.js
+++ app.js
@@ -12,10 +12,15 @@ const HEIGHT = canvas.height;
 // Wielkość pojedynczego kafelka (wyrażona w pikselach)
 const TILE_SIZE = 96;
 const SPEED = 8;
+const TEX_SPEED = SPEED * 4;
 
 // Wielkość mapy (wyrażona w kafelkach)
 const MAP_WIDTH = WIDTH / TILE_SIZE;
 const MAP_HEIGHT = HEIGHT / TILE_SIZE;
+
+// Offsety (renderowanie gracza na środku planszy)
+const xOffset = WIDTH / 2 - TILE_SIZE / 2;
+const yOffset = HEIGHT / 2 - TILE_SIZE / 2;
 
 // Tablica [x][y] z kafelkami
 var tiles = [];
@@ -24,6 +29,12 @@ var tiles = [];
 var playerX = 0;
 var playerY = 0;
 
+// Ruch gracza
+var direction = 0;
+var moving = false;
+var movingTime = 0;
+
+// Aktualna textura gracza
 var playerImg = createImage("player.png");
 
 // Rodzaje używanych kafelków
@@ -34,12 +45,28 @@ const tilesNames = [
 const tilesImages = [];
 const keys = {};
 
+// Tablica kierunków
+const dirs = [
+    "up", "down", "left", "right"
+];
+
+// Klawisze
+const movingKeys = [
+    "W", "S", "A", "D"
+];
+
+// Klawisze i kierunki poruszania
 const allTheRightMoves = {
     W: [0, -1],
     S: [0, 1],
     A: [-1, 0],
     D: [1, 0]
 };
+
+// Tablice tekstur
+const textures = [];
+const movingTextures1 = [];
+const movingTextures2 = [];
 
 function joinGame() {
     document.body.style.backgroundColor = "#121212"; // Zmiana koloru tła
@@ -66,8 +93,16 @@ function joinGame() {
 
 // Funkcja ładowania obrazków
 function loadImages() {
+    // Ładowanie kafelków
     for(var i = 0; i < tilesNames.length; i++) {
         tilesImages[i] = createImage(tilesNames[i] + ".png");
+    }
+
+    // Ładowanie tektur gracza
+    for(var dir of dirs) {
+        textures.push(createImage("player/" + dir + ".png"));
+        movingTextures1.push(createImage("player/" + dir + "_go1.png"));
+        movingTextures2.push(createImage("player/" + dir + "_go2.png"));
     }
 }
 
@@ -86,16 +121,22 @@ function initTiles() {
     }
 }
 
+// Funkcja poruszania się
+function move(x, y) {
+    playerX += x * SPEED;
+    playerY += y * SPEED;
+    playerImg = textures[direction];
+    moving = true;
+}
+
+// Funkcja renderująca
 function draw() {
     requestAnimationFrame(draw);
+    moving = false;
 
     // Czyszczenie ekranu
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
-
-    // Offsety (renderowanie gracza na środku planszy)
-    var xOffset = WIDTH / 2 - TILE_SIZE / 2;
-    var yOffset = HEIGHT / 2 - TILE_SIZE / 2;
 
     // Renderowanie Kafelków
     for(var x = 0; x < MAP_WIDTH; x++) {
@@ -113,15 +154,32 @@ function draw() {
     // Poruszanie się gracza
     for(var keyOfObj in keys) {
         var playerMove = allTheRightMoves[keyOfObj];
+        
         // Poruszaj, gry klawisz jest naciśnięty oraz jest to W,S,D lub D
         if(keys[keyOfObj] && playerMove) {
             // Poruszaj gracza
-            playerX += playerMove[0] * SPEED;
-            playerY += playerMove[1] * SPEED;
+            var moveX = playerMove[0];
+            var moveY = playerMove[1];
+            direction = movingKeys.indexOf(keyOfObj);
+            move(moveX, moveY);
         }
     }
+    setTexture();
 
-    console.log(playerX, playerY);
+    if(moving) {
+        movingTime++;
+    } else {
+        movingTime = 0;
+    }
+}
+
+// Funkcja ustawiająca teksturę
+function setTexture() {
+    if(moving) {
+        playerImg = (movingTime % TEX_SPEED < TEX_SPEED / 2) ? movingTextures1[direction] : movingTextures2[direction];
+    } else {
+        playerImg = textures[direction];
+    }
 }
 
 // Funkcja ładująca pojedynczy obrazek
